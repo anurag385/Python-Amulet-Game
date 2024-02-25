cec337717345c0fc51ae625fac6dabd828355285
Manti Yoshi Anurag <135608745+anurag385@users.noreply.github.com>
2024-02-25 20:46:32 +0530

    Create puzzle.py

---
+++ puzzle.py
@@ -0,0 +1,22 @@
+#**Python Puzzle Challenge:**
+
+# Ready to put your Python skills to the test? Solve this puzzle to unlock the hidden sanctuary of the Python Amulet!
+
+#📝 Instructions:
+#1. You'll be presented with a Python-related question.
+#2. Enter your answer to proceed.
+#3. If your answer is correct, you'll unlock the Python Amulet's sanctuary and claim victory!
+
+#💻 Python code:
+x = int(input("Enter the First Number shown in the game"))
+n = int(input("Enter the Second Number Shown in the game"))
+term_number = str(x)
+total = 0
+for i in range(1 , n + 1):
+    term = term_number * i
+    total += int(term)
+print(total)    
+#🎉 **Claim Your Victory:**
+
+#Think you've cracked the code? Enter your answer and see if you have what it takes to unlock the Python Amulet's sanctuary!
+
